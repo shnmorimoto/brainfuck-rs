90bc94bee35bbc041d98a002d3c2c0b3cc925c03
Shinichi Morimoto <shnmorimoto@gmail.com>
2019-09-30 23:33:29 +0900

    impl parser error

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,4 +1,5 @@
 use crate::lexer::token::Token;
+use std::fmt;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum ParseError {
@@ -6,4 +7,21 @@ pub enum ParseError {
     NotExpression(Token),
     UnclosedOpenParen(Token),
     Eof,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::ParseError::*;
+        match self {
+            UnexpectedToken(tok) => write!(f, "{}: {} is not expected", tok.loc, tok.value),
+            NotExpression(tok) => write!(
+                f,
+                "{}: '{}' is not a start of expression",
+                tok.loc, tok.value
+            ),
+            UnclosedOpenParen(tok) => write!(f, "{}: '{}' is not closed", tok.loc, tok.value),
+            Eof => write!(f, "End of file"),
+
+        }
+    }
 }
