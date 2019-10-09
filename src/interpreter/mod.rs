d4b37b294b54d0b49f2f61a4ef026569bb267f22
Shinichi Morimoto <shnmorimoto@gmail.com>
2019-10-09 23:50:54 +0900

    add initial eval

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,7 +1,17 @@
+use crate::parser::ast::Ast;
+use crate::parser::ast::AstKind;
+
+mod error;
+
 struct Interpreter;
 
 impl Interpreter {
+
     pub fn new() -> Self {
         Interpreter
+    }
+
+    pub fn eval(&mut self, expr: Vec<Ast>) -> () {
+        ()
     }
 }
