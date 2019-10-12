4a0fbdaa7b962e273912219bfd466c09e2d33958
Shinichi Morimoto <shnmorimoto@gmail.com>
2019-10-12 21:17:42 +0900

    impl inc, dec, next, prev patterns

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -4,15 +4,44 @@ use error::InterpreterError;
 
 mod error;
 
-struct Interpreter;
+struct Interpreter {
+    position: usize,
+    tape: Vec<u64>,
+    size: usize,
+}
 
 impl Interpreter {
 
-    pub fn new() -> Self {
-        Interpreter
+    pub fn new(tape_size: usize) -> Self {
+        Interpreter {
+            position: 0,
+            size: tape_size,
+            tape: vec![0; tape_size],
+        }
     }
 
-    pub fn eval(&mut self, expr: Vec<Ast>) -> () {
-        ()
+    pub fn eval(&mut self, expr: Vec<Ast>) -> Result<(), InterpreterError> {
+        for ast in expr.into_iter() {
+            match ast.value {
+                AstKind::Incr => self.tape[self.position] += 1,
+                AstKind::Decr => self.tape[self.position] -= 1,
+                AstKind::Next => { 
+                    if self.position + 1 >= self.size - 1 {
+                        return Err(InterpreterError::buffer_overflow(ast.loc))
+                    } else {
+                        self.position += 1
+                    }
+                }
+                AstKind::Prev => {
+                    if self.position - 1 < 0 {
+                        return Err(InterpreterError::negative_postion(ast.loc))
+                    } else {
+                        self.position -= 1
+                    }
+                }
+                _ => ()
+            }
+        }
+        Ok(())
     }
 }
