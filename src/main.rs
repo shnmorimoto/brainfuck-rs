04478266f52d3b866caa48f23b77781c91382e79
Shinichi Morimoto <shnmorimoto@gmail.com>
2019-10-14 21:58:33 +0900

    run abc

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use brainfuck_rs::interpreter::Interpreter;
+use brainfuck_rs::parser::parser::parse;
+use brainfuck_rs::lexer::lexer::lex;
+
+fn main() {
+    let mut interp = Interpreter::new(3000);
+    let code: &str = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.>++++++++++."; //ABC
+    let tokens = match lex(code) {
+        Ok(tokens) => tokens,
+        Err(_) => return (),
+    };
+    let asts = match parse(tokens) {
+        Ok(asts) => asts,
+        Err(_) => return (),
+    };
+    match interp.eval(asts) {
+        Ok(_) => return (),
+        Err(_) => return (),
+    }
+}
