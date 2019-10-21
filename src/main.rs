49dcb232fbc9e905f60a782be1179e1fd62309b2
Shinichi Morimoto <shnmorimoto@gmail.com>
2019-10-21 22:41:29 +0900

    add cmd option

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,63 @@
+use brainfuck_rs::error::show_trace;
+use brainfuck_rs::error::Error;
 use brainfuck_rs::interpreter::Interpreter;
-use brainfuck_rs::parser::parser::parse;
 use brainfuck_rs::lexer::lexer::lex;
 use brainfuck_rs::parser::ast::Ast;
-use brainfuck_rs::error::Error;
-use brainfuck_rs::error::show_trace;
-use std::env;
+use brainfuck_rs::parser::parser::parse;
 use std::fs::read_to_string;
 use std::process;
+extern crate clap;
+use clap::{App, Arg};
 
 fn main() {
-    let mut interp = Interpreter::new(3000);
+    const DEFAULT_TAPE_SIZE: usize = 30000;
 
-    let filename = env::args().nth(1).expect("1 argument FILENAME required");
+    let opts = App::new(env!("CARGO_PKG_NAME"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .arg(
+            Arg::with_name("FILENAME")
+                .short("f")
+                .long("file_name")
+                .help("brainfuck file name")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("TAPE_SIZE")
+                .short("s")
+                .long("tape_size")
+                .help("tape size")
+                .takes_value(true),
+        );
+
+    let matches = opts.get_matches();
+
+    let filename = match matches.value_of("FILENAME") {
+        Some(filename) => filename,
+        _ => {
+            println!("FILENAME required");
+            process::exit(-1);
+        }
+    };
+
+    let tape_size = match matches.value_of("TAPE_SIZE") {
+        Some(tape_size_str) => match tape_size_str.trim().parse::<usize>() {
+            Ok(tape_size) => tape_size,
+            _ => {
+                println!(
+                    "[Warn] parse error: {}. tape_size is set to default size: {}",
+                    tape_size_str, DEFAULT_TAPE_SIZE
+                );
+                DEFAULT_TAPE_SIZE
+            }
+        },
+        _ => DEFAULT_TAPE_SIZE,
+    };
+
+    let mut interp = Interpreter::new(tape_size);
+
     let code = match read_to_string(filename) {
         Ok(code) => code,
         Err(e) => {
@@ -19,7 +65,6 @@ fn main() {
             process::exit(-1);
         }
     };
-
 
     let asts = match parse_code(&code) {
         Ok(asts) => asts,
